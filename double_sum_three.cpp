6e475daca57facdead32ce5d5624c9956ee5dfa4
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-10 21:31:23 +0500

    Create double_sum_three.cpp

---
+++ double_sum_three.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int doubleSumThree(int a, int b, int c) {
+    return (a + b + c) * 2;
+}
+
+int main() {
+    cout << doubleSumThree(2, 3, 4);
+    return 0;
+}
